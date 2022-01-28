8da00cf90c3f6a9ae3d289c792e545688550717b
Bruno Moretto <bruno.moretto@pucrs.br>
2022-01-27 21:02:06 -0300

    Add Swagger to project and its configuration

---
+++ src/main/java/com/moretto/bruno/employee/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.moretto.bruno.employee.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String API_TITLE = "ws-employee";
+    private static final String API_DESCRIPTION = "A Rest API to manage employees of a company";
+    private static final String API_VERSION = "1.0.0";
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .build();
+    }
+
+}
