c82aff1dc45d874d451b512d2459fcb08ae74cf3
Bruno Moretto <bmoretto404@gmail.com>
2022-02-08 19:43:37 -0300

    Creation of method to search an employee by id

--- src/main/java/com/moretto/bruno/employee/service/EmployeeService.java
+++ src/main/java/com/moretto/bruno/employee/service/EmployeeService.java
@@ -2,15 +2,23 @@ package com.moretto.bruno.employee.service;
 
 import com.moretto.bruno.employee.dto.EmployeeDto;
 
+import java.util.Optional;
+
 /**
  * Service to deal with operation around employees
  */
 public interface EmployeeService {
 
     /**
+     * Searches by an employee in the database by your id
+     * @param id employee id to searches by
+     */
+    Optional<EmployeeDto> searchById(Long id);
+
+    /**
      * Save a new employee in the database
      * @param employeeDto employee to be saved
      */
-    void create(EmployeeDto employeeDto);
+    EmployeeDto create(EmployeeDto employeeDto);
 
 }
--- src/main/java/com/moretto/bruno/employee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/moretto/bruno/employee/service/impl/EmployeeServiceImpl.java
@@ -2,14 +2,19 @@ package com.moretto.bruno.employee.service.impl;
 
 import com.moretto.bruno.employee.dto.EmployeeDto;
 import com.moretto.bruno.employee.entity.Employee;
+
+import static com.moretto.bruno.employee.mapper.EmployeeMapper.toDto;
 import static com.moretto.bruno.employee.mapper.EmployeeMapper.toEntity;
 
+import com.moretto.bruno.employee.mapper.EmployeeMapper;
 import com.moretto.bruno.employee.repository.EmployeeRepository;
 import com.moretto.bruno.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -18,10 +23,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
 
     @Override
-    public void create(EmployeeDto employeeDto) {
+    public Optional<EmployeeDto> searchById(Long id) {
+        Optional<Employee> entityOpt = employeeRepository.findById(id);
+
+        return entityOpt.isPresent() ? entityOpt.map(EmployeeMapper::toDto) : Optional.empty();
+    }
+
+    @Override
+    public EmployeeDto create(EmployeeDto employeeDto) {
         try {
             Employee entity = toEntity(employeeDto);
-            employeeRepository.save(entity);
+            entity = employeeRepository.save(entity);
+            return toDto(entity);
         } catch (Exception e) {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR,
